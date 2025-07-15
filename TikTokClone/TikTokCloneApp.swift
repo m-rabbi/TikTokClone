4f5d1838a95f2e95694c3325ce93b3a0a9acc118
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-15 13:40:02 -0400

    chore(init): first commit for TikTokClone project

---
+++ TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Md Rabbi on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
