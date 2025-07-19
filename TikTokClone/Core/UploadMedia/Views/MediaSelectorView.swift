b699bd19c3b8cc79aea5dd673131f0db76cd6ecb
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-19 14:46:53 -0400

    feat(post): add video picker and preview with AVPlayer using Transferable Movie

--- TikTokClone/Core/UploadMedia/Views/MediaSelectorView.swift
+++ TikTokClone/Core/UploadMedia/Views/MediaSelectorView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 import PhotosUI
 
 struct MediaSelectorView: View {
+    @State private var player = AVPlayer()
     @State private var showMediaPicker: Bool = false
     @State private var selectedItem: PhotosPickerItem?
+    @State private var mediaPreview: Movie?
     
     var body: some View {
         NavigationStack {
             VStack {
-                
+                if let mediaPreview {
+                    CustomVideoPlayer(player: player)
+                        .onAppear { player.replaceCurrentItem(with: .init(url: mediaPreview.url))
+                            player.play()
+                        }
+                        .padding()
+                }
+            }
+            .task(id: selectedItem) {
+                await loadMediaPreview(fromItem: selectedItem)
             }
             .navigationTitle("New Post")
             .navigationBarTitleDisplayMode(.inline)
@@ -29,6 +40,15 @@ struct MediaSelectorView: View {
                 }
             }
         }
+    }
+}
+
+private extension MediaSelectorView {
+    func loadMediaPreview(fromItem item: PhotosPickerItem?) async {
+        guard let item else { return }
+        
+        self.mediaPreview =  try? await item.loadTransferable(type: Movie.self)
+        
     }
 }
 
