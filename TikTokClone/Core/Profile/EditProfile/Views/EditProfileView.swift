0415325fa4275f55a148adc4edd654764e2ecdeb
Md Rabbi <mdfrabbi1975@gmail.com>
2025-07-19 06:17:10 -0400

    feat(profile): load and display selected image in EditProfileView using PhotosPicker

--- TikTokClone/Core/Profile/EditProfile/Views/EditProfileView.swift
+++ TikTokClone/Core/Profile/EditProfile/Views/EditProfileView.swift
@@ -6,19 +6,34 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct EditProfileView: View {
+    @State private var selectedPickerItem: PhotosPickerItem?
+    @State private var profileImage: Image?
+    
     var body: some View {
         NavigationStack {
             VStack {
-                VStack {
-                    Circle()
-                        .frame(width: 64, height: 64)
-                    
-                    Button("Change photo") {
-                        print("DEBUG: Change photo")
+                PhotosPicker(selection: $selectedPickerItem, matching: .images) {
+                    VStack {
+                        if let image = profileImage {
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 64, height: 64)
+                                .clipShape(Circle())
+                        } else {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 64, height: 64)
+                                .clipShape(Circle())
+                                .foregroundStyle(Color(.systemGray4))
+                        }
+                        
+                        Text("Change photo")
                     }
-                    .foregroundStyle(.black)
                 }
                 .padding()
                 
@@ -52,30 +67,43 @@ struct EditProfileView: View {
                         Text("Add a bio")
                     }
                 }
+                .task(id: selectedPickerItem) {
+                    await loadImage(fromItem: selectedPickerItem)
+                }
                 .font(.subheadline)
                 .padding()
                 
                 Spacer()
-            }
-            .navigationTitle("Edit Profile")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button("Cancel") {
-                        print("DEBUG: Cancel")
+                    .navigationTitle("Edit Profile")
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Button("Cancel") {
+                                print("DEBUG: Cancel")
+                            }
+                            .foregroundStyle(.black)
+                        }
+                        
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button("Done") {
+                                print("DEBUG: Cancel")
+                            }
+                            .fontWeight(.semibold)
+                            .foregroundStyle(.black)
+                        }
                     }
-                    .foregroundStyle(.black)
-                }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Done") {
-                        print("DEBUG: Cancel")
-                    }
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.black)
-                }
             }
         }
+    }
+}
+
+extension EditProfileView {
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.profileImage = Image(uiImage: uiImage)
     }
 }
 
